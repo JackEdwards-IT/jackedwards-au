bb11c965a2898aa29d3bc47030b5000e88bd086f
Jack Edwards <admin@jackedwards.au>
2023-05-30 06:57:41 +1000

    Bump dependencies

--- pages/index.vue
+++ pages/index.vue
@@ -4,10 +4,14 @@
   <div class="bg-red w-full h-full">
     <div class="m-4">
       <h1 class="text-3xl mb-4">Hi, I'm Jack Edwards,</h1>
-      <h1 class="text-3xl">IT professional working as a frontend developer.</h1>
+      <h1 class="text-3xl">Software Developer.</h1>
 
       <h2 class="mt-4 text-xl">
-        I am currently rebuilding this site with Nuxt3, stay tuned.
+        Currently building progressive web apps with Vue 3 and Nuxt 3.
+      </h2>
+      <h2 class="text-xl">
+        I enjoy working on all aspects of modern web stacks and creating
+        accessible and performant products.
       </h2>
     </div>
   </div>
